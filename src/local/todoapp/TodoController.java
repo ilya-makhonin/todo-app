4da1ae6944f8832a40fcdd1512bd7b76505e181e
arty <arty@123.com>
2019-10-31 21:05:07 -0400

    second commit

---
+++ src/local/todoapp/TodoController.java
@@ -0,0 +1,17 @@
+package local.todoapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.ModelMap;
+
+@Controller
+@RequestMapping("/hello")
+
+public class TodoController {
+    @RequestMapping(method = RequestMethod.GET)
+    public String printHello(ModelMap model) {
+        model.addAttribute("message", "Hello Spring MVC Framework!");
+        return "hello";
+    }
+}
